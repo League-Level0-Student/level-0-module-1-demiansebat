999d3d5a089e13a6037d88c4cbfa283becf8732b
demiansebat <league@WTS14-4.attlocal.net>
2018-10-05 18:35:52 -0700

    Unbirthday,Remarkable

---
+++ src/ifs_ints_and_loops/Remarkable.java
@@ -0,0 +1,9 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("Write your name in the box below.");
+	}
+}
--- src/ifs_ints_and_loops/Unbirthday.java
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -1,5 +1,15 @@
 package ifs_ints_and_loops;
 
-public class Unbirthday {
+import javax.swing.JOptionPane;
 
+public class Unbirthday {
+	public static void main(String[] args) {
+		String LOL = JOptionPane.showInputDialog("What is the date of your birthday?");
+		if (LOL.equals("10/5")) {
+			JOptionPane.showMessageDialog(null, "Have a very happy birthday!");
+
+		} else {
+			JOptionPane.showMessageDialog(null, "Have a very sad unbirthday :(");
+		}
+	}
 }
