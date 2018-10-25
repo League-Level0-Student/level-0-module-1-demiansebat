2306da8cfb6ce2fefec7a435924f98e63574827e
demiansebat <league@WTS14-4.attlocal.net>
2018-10-24 18:23:45 -0700

    remarkable, not finished secret message box

--- src/ifs_ints_and_loops/Remarkable.java
+++ src/ifs_ints_and_loops/Remarkable.java
@@ -5,21 +5,23 @@ import javax.swing.JOptionPane;
 public class Remarkable {
 
 	public static void main(String[] args) {
-		String zack= "very, very remarkable";
-		String miceal="super remarkable";
-		String datdude ="more remarkable than miceal";
+		String zack = "very, very remarkable";
+		String miceal = "super remarkable";
+		String datdude = "more remarkable than miceal";
 		String HAHA = JOptionPane.showInputDialog("Write your name in the box below.");
-		if(HAHA.equals("miceal")) {
-		JOptionPane.showMessageDialog(null, miceal);
+		if (HAHA.equals("miceal")) {
+			JOptionPane.showMessageDialog(null, miceal);
 		}
-	
-        if(HAHA.equals(zack)) {
-JOptionPane.showMessageDialog(null,"zack");
-        }
-        
-        if(HAHA.equals("datdude")) {
-JOptionPane.showMessageDialog(null,"datdude");
-        }
-}
-}
+
+		if (HAHA.equals("zack")) {
+			JOptionPane.showMessageDialog(null, zack);
+		}
+
+		if (HAHA.equals("datdude")) {
+			JOptionPane.showMessageDialog(null, datdude);
+		}
+		if (HAHA.equals("miceal")) {
+			JOptionPane.showMessageDialog(null, miceal);
+		}
+	}
 }
