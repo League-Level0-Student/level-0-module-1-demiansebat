44d9f24b2cceed30106d037e82a4370779adfc57
League <league@WTS9.attlocal.net>
2018-10-28 12:57:53 -0700

    Secret Message Box and Unfinished riddler

--- src/ifs_ints_and_loops/SecretMessageBox.java
+++ src/ifs_ints_and_loops/SecretMessageBox.java
@@ -19,8 +19,15 @@ public class SecretMessageBox {
 	// 0. Make a main method and put steps 1-5 inside it
 	public static void main(String[] args) {
 		String PASSWORD = ("12345");
-		JOptionPane.showInputDialog(null, "")
+		 String SecretMessage=" I stole 1,000,000 dollars from the bank.";
+		 JOptionPane.showMessageDialog(null, "You can only see the secret message if you guess the password.");
+		String UserPassword= JOptionPane.showInputDialog("What is the password?");
+		if(UserPassword.equals("12345")) {
+				JOptionPane.showMessageDialog(null, "Hello the secret message is"+SecretMessage);
 	}
+		else {
+			JOptionPane.showMessageDialog(null, "INTRUDER!!! GO AWAY OR ELSE :(");
+		}
 	// 1. Set a password in a String variable
 
 	// 2. Using a pop-up, ask the first person for a secret message and store it in0
@@ -34,4 +41,4 @@ public class SecretMessageBox {
 
 	// 5. If the password does not match, pop-up "INTRUDER!!"
 
-}
+}}
--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -1,5 +1,8 @@
 package ifs_ints_and_loops;
-//    Copyright (c) The League of Amazing Programmers 2013-2017
+
+import javax.print.attribute.standard.JobKOctetsProcessed;
+import javax.swing.JOptionPane;
+
 //    Level 0
 
 
@@ -7,11 +10,15 @@ package ifs_ints_and_loops;
 public class TheRiddler {
 
 	public static void main(String[] args) {
+		int SCORE=0;
 
 		// 1. Make a variable to hold the score
-
+String Riddle1=JOptionPane.showInputDialog("Imagine you are in a dark room with no windows doors or any openings. How do you get out");
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+if( Riddle1.equals("Stop imagining")){
+	JOptionPane.showMessageDialog(null, "Correct!Now your score is 1");
+	SCORE=SCORE +1;
+}
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 
 		// 5. Otherwise, say "wrong" and tell them the answer
